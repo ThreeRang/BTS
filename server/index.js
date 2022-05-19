c842c1a40da952f95f18f461a82833db3c5c3e0e
songjaesong <wotjs1007@naver.com>
2022-05-19 23:58:43 +0900

    [feat] 공연 등록기능 추가

--- server/index.js
+++ server/index.js
@@ -40,8 +40,9 @@ const connect = async () => {
     app.use('/api/update', require('./routes/update')); */
     //use this to show the image you have in node js server to client (react js)
     //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+    app.use("/image", express.static("image"));
+    app.use("/api/upload", require("./routes/upload"));
     app.use("/api/users", require("./routes/users"));
-    app.use("/uploads", express.static("uploads"));
 
     // Serve static assets if in production
     /*  if (process.env.NODE_ENV === "production") {
