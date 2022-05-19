095472352f2dd10431cd4256064ca03c94ecf674
songjaesong <wotjs1007@naver.com>
2022-05-19 20:06:17 +0900

    [feat] concert schema작성 && react-bootstrap설치

--- server/models/Concert.js
+++ server/models/Concert.js
@@ -5,45 +5,55 @@ const concertSchema = mongoose.Schema(
     _id: {
       type: String,
     },
-    title: {
-      //제목
-      type: String,
-      maxlength: 50,
-      default: "none",
-    },
-    concertDate: {
-      //공연날짜
-      type: Date,
-      default: Date.now,
-    },
-    ownerId: {
-      //공연등록자 Id
-      type: String,
-      default: "none",
-    },
-    numOfSeat: {
-      //공연좌석수
-      type: Number,
-      default: 0,
-    },
-    reservationOpen: {
-      //예매오픈
-      type: Date,
-      default: Date.now,
-    },
-    reservationClose: {
-      //예매기한
-      type: Date,
-      default: Date.now,
+    concertInfo: {
+      _id: {
+        type: String,
+      },
+      title: {
+        //제목
+        type: String,
+        maxlength: 50,
+        default: "제목없음",
+      },
+      description: {
+        //공연설명
+        type: String,
+        default: "none",
+      },
+      concertDate: {
+        //공연날짜
+        type: String,
+        default: Date.now,
+      },
+      numOfSeat: {
+        //공연좌석수
+        type: Number,
+        default: 0,
+      },
+      reservation: {
+        open: {
+          type: Date,
+          default: Date.now,
+        },
+        close: {
+          type: Date,
+          default: Date.now,
+        },
+      },
     },
     image: {
-      type: String,
-      default: "",
-    },
-    description: {
-      //공연설명
-      type: String,
-      default: "none",
+      concertImage: {
+        type: String,
+        default: "",
+      },
+      ticketImage: {
+        type: String,
+        default: "",
+      },
+      seatImage: {
+        type: String,
+        default: "",
+      },
     },
   },
   { timestamps: true }
