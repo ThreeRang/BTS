38b44816b186d9c8858225ed24f15eabfd74bf1d
songjaesong <wotjs1007@naver.com>
2022-05-21 23:07:02 +0900

    [feat] 정렬기능추가

--- server/routes/concert.js
+++ server/routes/concert.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const { Concert } = require("../models/Concert");
 
 router.get("/getConcerts", (req, res) => {
-  const year = new Date().getFullYear();
+  const { search, sort } = req.query;
+  /* const year = new Date().getFullYear();
   let month = new Date().getMonth();
   let day = new Date().getDate();
   if (month < 10) {
@@ -12,28 +13,75 @@ router.get("/getConcerts", (req, res) => {
   if (day < 10) {
     day = "0" + day;
   }
-  const today = year + "-" + month + "-" + day;
-  if (req.query.search === "") {
-    Concert.find(/* {
-    today: {
-      $gte: "concertInfo.reservation.open.date",
-      $lte: "concertInfo.reservation.close.date",
-    },
-  } */)
-      .sort({ "concertInfo.concertDate.date": "1" })
-      .exec((err, concerts) => {
-        if (err) return res.status(400).json({ success: false, err });
-        return res.status(200).json({ success: true, concerts });
-      });
+  const today = year + "-" + month + "-" + day; */
+  if (search === "") {
+    if (sort == 0) {
+      Concert.find()
+        .sort({ "concertInfo.concertDate.date": "1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    } else if (sort == 1) {
+      Concert.find()
+        .sort({ createdAt: "-1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    } else if (sort == 2) {
+      Concert.find()
+        .sort({ "concertInfo.concertTitle": "1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    } else {
+      Concert.find()
+        .sort({ "concertInfo.reservation.close": "1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    }
   } else {
-    Concert.find({
-      "concertInfo.concertTitle": { $regex: req.query.search, $options: "i" },
-    })
-      .sort({ "concertInfo.concertDate.date": "1" })
-      .exec((err, concerts) => {
-        if (err) return res.status(400).json({ success: false, err });
-        return res.status(200).json({ success: true, concerts });
-      });
+    if (sort === 0) {
+      Concert.find({
+        "concertInfo.concertTitle": { $regex: search, $options: "i" },
+      })
+        .sort({ "concertInfo.concertDate.date": "1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    } else if (sort === 1) {
+      Concert.find({
+        "concertInfo.concertTitle": { $regex: search, $options: "i" },
+      })
+        .sort({ createdAt: "-1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    } else if (sort === 2) {
+      Concert.find({
+        "concertInfo.concertTitle": { $regex: search, $options: "i" },
+      })
+        .sort({ "concertInfo.concertTitle": "1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    } else {
+      Concert.find({
+        "concertInfo.concertTitle": { $regex: search, $options: "i" },
+      })
+        .sort({ "concertInfo.reservation.close": "1" })
+        .exec((err, concerts) => {
+          if (err) return res.status(400).json({ success: false, err });
+          return res.status(200).json({ success: true, concerts });
+        });
+    }
   }
 });
 module.exports = router;
