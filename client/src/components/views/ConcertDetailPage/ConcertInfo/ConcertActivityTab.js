cd659b6ab2685e246267752c432f6b59700db87e
songjaesong <wotjs1007@naver.com>
2022-11-03 23:16:10 +0900

    [feat] activity tab table init

--- client/src/components/views/ConcertDetailPage/ConcertInfo/ConcertActivityTab.js
+++ client/src/components/views/ConcertDetailPage/ConcertInfo/ConcertActivityTab.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card } from 'antd-v3';
+import './ConcertActivityTab.css';
 
-const ConcertActivityTab = () => {
-  return <div>ConcertActivityTab</div>;
+const ConcertActivityTab = ({ concertId }) => {
+  const [transaction, setTransaction] = useState([]);
+
+  useEffect(() => {
+    /**  */
+  }, []);
+
+  return (
+    <div class="wrapper">
+      <Card title={`concertId : ${concertId}`} size="small">
+        <table>
+          <thead>
+            <tr>
+              <th>from </th>
+              <th>to </th>
+              <th>price </th>
+              <th>date </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>안녕</td>
+              <td>안녕하세요</td>
+              <td>hihi</td>
+              <td>봉쥬르</td>
+            </tr>
+            <tr>
+              <td>안녕</td>
+              <td>안녕하세요</td>
+              <td>hihi</td>
+              <td>봉쥬르</td>
+            </tr>
+            <tr>
+              <td>안녕</td>
+              <td>안녕하세요</td>
+              <td>hihi</td>
+              <td>봉쥬르</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <br />
+      </Card>
+    </div>
+  );
 };
 
 export default ConcertActivityTab;
