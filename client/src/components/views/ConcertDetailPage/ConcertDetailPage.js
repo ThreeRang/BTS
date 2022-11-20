911ac92490ceb59e9c4b27c143f7ef9bd87053f0
songjaesong <wotjs1007@naver.com>
2022-11-20 15:25:40 +0900

    [refactor] update ConcertDetailPage

--- client/src/components/views/ConcertDetailPage/ConcertDetailPage.js
+++ client/src/components/views/ConcertDetailPage/ConcertDetailPage.js
@@ -5,40 +5,42 @@ import { Tabs } from 'antd-v3';
 import ConcertDetailTab from './ConcertInfo/ConcertDetailTab';
 import ConcertItemsTab from './ConcertInfo/ConcertItemsTab';
 import ConcertActivityTab from './ConcertInfo/ConcertActivityTab';
+import useConcert from '../../../hooks/useConcert';
 
 import detailPageStyle from './ConcertDetailPage.module.css';
 const { TabPane } = Tabs;
 const ConcertDetailPage = () => {
   const { concertId } = useParams();
-  const [imageHash, setImageHash] = useState('');
-  useEffect(() => {
-    Axios.get('http://localhost:5000/api/concert/getConcertInfo', { params: { _id: concertId } }).then((response) => {
-      if (response.data.success) {
-        setImageHash(response.data.concert.image.imageHash);
-      } else {
-        alert('공연 정보를 읽는데 실패하였습니다.');
-      }
-    });
-  }, [concertId]);
+  const concert = useConcert({ concertId });
+
   return (
-    <div className={detailPageStyle.wrapper}>
-      <div className={detailPageStyle.profileImage}>
-        <img src={`https://ipfs.io/ipfs/${imageHash}/concertImage.jpg`} alt="concertimage" />
-      </div>
-      <div>
-        <Tabs defaultActiveKey="1">
-          <TabPane tab="Detail" key="1">
-            <ConcertDetailTab concertId={concertId} />
-          </TabPane>
-          <TabPane tab="Items" key="2">
-            <ConcertItemsTab concertId={concertId} />
-          </TabPane>
-          <TabPane tab="Activity" key="3">
-            <ConcertActivityTab concertId={concertId} />
-          </TabPane>
-        </Tabs>
-      </div>
-    </div>
+    <>
+      {concert.isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className={detailPageStyle.wrapper}>
+          <div className={detailPageStyle.profileImage}>
+            <img
+              src={`https://ipfs.io/ipfs/${concert.concertMetadata.image.imageHash}/concertImage.jpg`}
+              alt="concertimage"
+            />
+          </div>
+          <div>
+            <Tabs defaultActiveKey="1">
+              <TabPane tab="Detail" key="1">
+                <ConcertDetailTab concertId={concertId} />
+              </TabPane>
+              <TabPane tab="Items" key="2">
+                <ConcertItemsTab concertId={concertId} />
+              </TabPane>
+              <TabPane tab="Activity" key="3">
+                <ConcertActivityTab concertId={concertId} />
+              </TabPane>
+            </Tabs>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
